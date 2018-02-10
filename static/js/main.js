38a1454d3c264b052a637e2d7846196862a29bad
ironmanciti <ironmanciti@gmail.com>
2018-02-11 00:19:52 +0900

    quite revised

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,34 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event){
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+
+$(document).ready(function() {
+
+var offset = 250;
+
+var duration = 300;
+
+$(window).scroll(function() {
+
+if ($(this).scrollTop() > offset) {
+
+$(".back-to-top").fadeIn(duration);
+
+} else {
+
+$(".back-to-top").fadeOut(duration);
+
+}
+
+});
+$(".back-to-top").click(function(event) {
+
+event.preventDefault();
+
+$("html, body").animate({scrollTop: 0}, duration);
+
+return false;
+
+})
+
+});

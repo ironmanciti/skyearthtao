98044350c019d460363cf7e8962cccd843def34c
ironmanciti <ironmanciti@gmail.com>
2018-03-30 04:02:33 +0900

    aaa

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, static_url_path='/')
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route("/")
 @app.route("/home/")
